45678fa9216e32b218fe1a7c03d4aab50534017d
Rezwan Huq <rezwan.huq@cellosco.pe>
2023-06-04 16:55:07 +0600

    add routing

--- world_time/lib/pages/choose_location.dart
+++ world_time/lib/pages/choose_location.dart
@@ -10,8 +10,15 @@ class ChooseLocation extends StatefulWidget {
 class _ChooseLocationState extends State<ChooseLocation> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Choose Location'),
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        // ignore: prefer_const_constructors
+        title: Text('Choose Location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
     );
   }
 }
