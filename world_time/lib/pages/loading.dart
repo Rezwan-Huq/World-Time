42c47d6e72db3c79834bcd2bda0ef43df75c4c57
Rezwan Huq <nightfreak70@gmail.com>
2023-06-18 22:49:35 +0600

    1122

--- world_time/lib/pages/loading.dart
+++ world_time/lib/pages/loading.dart
@@ -17,7 +17,23 @@ class _LoadingState extends State<Loading> {
     String url = "https://run.mocky.io/v3/a076a37f-4405-4f22-a8b9-d54d63c1fa82";
     final response = await http.get(Uri.parse(url));
     Map data = jsonDecode(response.body);
-    print(data);  
+    // print(data); 
+    // get properties from data
+
+    String datetime = data['utc_datetime'];
+    String offset = data['utc_offset'].substring(1,3); 
+
+    // print(datetime);
+    // print(offset);
+
+    // create datetime object 
+    DateTime now = DateTime.parse(datetime);
+    now =   now.add(Duration(hours: int.parse(offset)));
+    print(now);
+
+
+    
+
   }
 
   @override
